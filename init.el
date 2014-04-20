9baec0fb4cc543a2f0e205e522109dcfe40b2885
Ben Ashford <ben@bcash.org>
2014-04-20 17:36:19 +0100

    Tabs, themes, etc.

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;; Packages
 
 (setq package-list '(ac-nrepl
-					 ack-and-a-half
+                     ack-and-a-half
                      anti-zenburn-theme
                      auto-complete
                      cider
@@ -15,7 +15,7 @@
                      rainbow-delimiters
                      rbenv
                      undo-tree
-					 yaml-mode))
+                     yaml-mode))
 
 (require 'package)
 (add-to-list 'package-archives
@@ -41,8 +41,9 @@
 
 ;; THEMES
 
-(unless (equal "xterm-256color" (tty-type))
-    (load-theme 'anti-zenburn t))
+;; no theme
+;; (unless (equal "xterm-256color" (tty-type))
+;;     (load-theme 'anti-zenburn t))
 
 ;; FONTS
 
@@ -169,6 +170,7 @@
 
 ;; Tabs and stuff
 (setq default-tab-width 4)
+(setq-default indent-tabs-mode nil)
 
 ;; smex
 
@@ -195,8 +197,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("375cb420cbbe427fb84a21915f8b271978db9aa70513fd71e85fc7047faca879" "ee6081af57dd389d9c94be45d49cf75d7d737c4a78970325165c7d8cb6eb9e34" "e7af2246eff56872b197ee7493563a5ffa26022ca813b02c28c8e9f19f742678" "1989847d22966b1403bab8c674354b4a2adf6e03e0ffebe097a6bd8a32be1e19" "a774c5551bc56d7a9c362dca4d73a374582caedb110c201a09b410c0ebbb5e70" "bf648fd77561aae6722f3d53965a9eb29b08658ed045207fe32ffed90433eb52" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
- '(uniquify-buffer-name-style (quote post-forward) nil (uniquify)))
+ '(custom-safe-themes (quote ("1f70ca6096c886ca2a587bc10e2e8299ab835a1b95394a5f4e4d41bb76359633" "375cb420cbbe427fb84a21915f8b271978db9aa70513fd71e85fc7047faca879" "ee6081af57dd389d9c94be45d49cf75d7d737c4a78970325165c7d8cb6eb9e34" "e7af2246eff56872b197ee7493563a5ffa26022ca813b02c28c8e9f19f742678" "1989847d22966b1403bab8c674354b4a2adf6e03e0ffebe097a6bd8a32be1e19" "a774c5551bc56d7a9c362dca4d73a374582caedb110c201a09b410c0ebbb5e70" "bf648fd77561aae6722f3d53965a9eb29b08658ed045207fe32ffed90433eb52" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(uniquify-buffer-name-style (quote post-forward) nil (uniquify))
+)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
