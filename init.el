355e75ed2b9b1966151036d4b6855108e0f0614b
Ben Ashford <ben.ashford@skimlinks.com>
2014-04-24 17:28:51 +0100

    Add magit

--- init.el
+++ init.el
@@ -11,6 +11,7 @@
                      flx-ido
                      flycheck
                      git-gutter
+                     magit
                      paredit
                      projectile
                      smartparens
