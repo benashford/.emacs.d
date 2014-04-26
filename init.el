f728e7b86e360d10a7a5342834bad1c80279f7f8
Ben Ashford <ben@bcash.org>
2014-04-26 17:10:23 +0100

    Don't copy to OSX if Emacs is running under Tmux

--- init.el
+++ init.el
@@ -268,8 +268,9 @@
       (process-send-string proc text)
       (process-send-eof proc))))
 
-(setq interprogram-cut-function 'paste-to-osx)
-(setq interprogram-paste-function 'copy-from-osx)
+(unless (getenv "TMUX")
+  (setq interprogram-cut-function 'paste-to-osx)
+  (setq interprogram-paste-function 'copy-from-osx))
 
 ;; DO NOT EDIT
 (custom-set-variables
